d3619a94dad201e48a1a94bba0f22aeee36072cc
Pavel Klos <pavelkloscz@gmail.com>
2023-06-16 18:50:20 +0200

    Add registerAttendee Mutation

---
+++ GraphQL/Attendees/AttendeeMutations.cs
@@ -0,0 +1,29 @@
+﻿using GraphQL.Data;
+
+namespace GraphQL.Attendees
+{
+    [ExtendObjectType(Name = "Mutation")]
+    public class AttendeeMutations
+    {
+        [UseApplicationDbContext]
+        public async Task<RegisterAttendeePayload> RegisterAttendeeAsync(
+            RegisterAttendeeInput input,
+            [ScopedService] ApplicationDbContext context,
+            CancellationToken cancellationToken)
+        {
+            var attendee = new Attendee
+            {
+                FirstName = input.FirstName,
+                LastName = input.LastName,
+                UserName = input.UserName,
+                EmailAddress = input.EmailAddress
+            };
+
+            context.Attendees.Add(attendee);
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return new RegisterAttendeePayload(attendee);
+        }
+    }
+}
---
+++ GraphQL/Attendees/RegisterAttendeePayload.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Common;
+using GraphQL.Data;
+
+namespace GraphQL.Attendees
+{
+    public class RegisterAttendeePayload : AttendeePayloadBase
+    {
+        public RegisterAttendeePayload(Attendee attendee)
+            : base(attendee)
+        {
+        }
+
+        public RegisterAttendeePayload(UserError error)
+            : base(new[] { error })
+        {
+        }
+    }
+}
