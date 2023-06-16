f53cee4ee4fd6ff65e37d99844a584ed2ed2655b
Pavel Klos <pavelkloscz@gmail.com>
2023-06-16 20:52:04 +0200

    Add simple query tests

--- GraphQL.Tests/AttendeeTests.cs
+++ GraphQL.Tests/AttendeeTests.cs
@@ -35,5 +35,46 @@ namespace GraphQL.Tests
             // ASSERT
             schema.Print().MatchSnapshot();
         }
+
+        [Fact]
+        public async Task Register_Attendee()
+        {
+            // ARRANGE
+            IRequestExecutor executor = await new ServiceCollection()
+                .AddPooledDbContextFactory<ApplicationDbContext>(
+                    options => options.UseInMemoryDatabase("Data Source=conferences.db"))
+                .AddGraphQL()
+                .AddQueryType(d => d.Name("Query"))
+                    .AddTypeExtension<AttendeeQueries>()
+                .AddMutationType(d => d.Name("Mutation"))
+                    .AddTypeExtension<AttendeeMutations>()
+                .AddType<AttendeeType>()
+                .AddType<SessionType>()
+                .AddType<SpeakerType>()
+                .AddType<TrackType>()
+                //.EnableRelaySupport() // OBSOLETE
+                .AddGlobalObjectIdentification() // .EnableRelaySupport()
+                .BuildRequestExecutorAsync();
+
+            // ACT
+            IExecutionResult result = await executor.ExecuteAsync(@"
+        mutation RegisterAttendee {
+            registerAttendee(
+                input: {
+                    emailAddress: ""michael@chillicream.com""
+                        firstName: ""michael""
+                        lastName: ""staib""
+                        userName: ""michael3""
+                    })
+            {
+                attendee {
+                    id
+                }
+            }
+        }");
+
+            // ASSERT
+            result.ToJson().MatchSnapshot();
+        }
     }
 }
